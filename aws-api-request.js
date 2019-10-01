74c56d0e639e1a52b8081bcd1a9a00298593d939
Einar Egilsson <einar@einaregilsson.com>
2019-10-01 14:23:04 +0000

    First version

---
+++ aws-api-request.js
@@ -0,0 +1,154 @@
+const crypto = require('crypto'),
+    https = require('https'),
+    zlib = require('zlib');
+
+function awsApiRequest(options) {
+    return new Promise((resolve, reject) => {
+        let region = options.region || awsApiRequest.region || process.env.AWS_DEFAULT_REGION,
+            service = options.service,
+            accessKey = options.accessKey || awsApiRequest.accessKey || process.env.AWS_ACCESS_KEY_ID,
+            secretKey = options.secretKey || awsApiRequest.secretKey || process.env.AWS_SECRET_SECRET_KEY,
+            method = options.method || 'GET',
+            path = options.path || '/',
+            querystring = options.querystring || {},
+            payload = options.payload || '',
+            host = options.host || `${service}.${region}.amazonaws.com`,
+            headers = options.headers || {};
+
+        function hmacSha256(data, key, hex=false) {
+            return crypto.createHmac('sha256', key).update(data).digest(hex ? 'hex' : undefined);
+        }
+        
+        function sha256(data) {
+            return crypto.createHash('sha256').update(data).digest('hex');
+        }
+        
+        //Thanks to https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-javascript
+        function createSigningKey(secretKey, dateStamp, region, serviceName) {
+            let kDate = hmacSha256(dateStamp, 'AWS4' + secretKey);
+            let kRegion = hmacSha256(region, kDate);
+            let kService = hmacSha256(serviceName, kRegion);
+            let kSigning = hmacSha256('aws4_request', kService);
+            return kSigning;
+        }
+        
+        function createSignedHeaders(headers) {
+            return Object.keys(headers).sort().map(h => h.toLowerCase()).join(';');
+        }
+        
+        function createStringToSign(timestamp, region, service, canonicalRequest) {
+            let stringToSign = 'AWS4-HMAC-SHA256\n';
+            stringToSign += timestamp + '\n';
+            stringToSign += timestamp.substr(0,8) + '/' + region + '/' + service + '/aws4_request\n';
+            stringToSign += sha256(canonicalRequest);
+            return stringToSign;
+        }
+        
+        function createCanonicalRequest(method, uri, querystring, headers, payload) {
+            let canonical = method + '\n';
+            canonical += encodeURI(encodeURI(uri)) + '\n';
+        
+            let qsKeys = Object.keys(querystring);
+            qsKeys.sort();
+            let qsEntries = qsKeys.map(k => `${k}=${encodeURIComponent(querystring[k])}`);
+            canonical += qsEntries.join('&') + '\n';
+        
+            let headerKeys = Object.keys(headers).sort();
+            let headerEntries = headerKeys.map(h => h.toLowerCase() + ':' + headers[h].replace(/^\s*|\s*$/g, '').replace(' +', ' '));
+            canonical += headerEntries.join('\n') + '\n\n';
+        
+            canonical += createSignedHeaders(headers) + '\n';
+            canonical += sha256(payload);
+        
+            return canonical;
+        }
+        
+        function createAuthHeader(accessKey, timestamp, region, service, headers, signature) {
+            let date = timestamp.substr(0,8);
+            let signedHeaders = createSignedHeaders(headers);
+            return `AWS4-HMAC-SHA256 Credential=${accessKey}/${date}/${region}/${service}/aws4_request, SignedHeaders=${signedHeaders}, Signature=${signature}`;
+        }
+
+        let timestamp = new Date().toISOString().replace(/(-|:|\.\d\d\d)/g, ''); // YYYYMMDD'T'HHmmSS'Z'
+        let datestamp = timestamp.substr(0,8);
+
+        let reqHeaders = Object.assign({
+            Accept : 'application/json',
+            Host : host,
+            'Content-Type' : 'application/json',
+            'x-amz-date' : timestamp,
+            'x-amz-content-sha256' : sha256(payload)
+        }, headers); // Passed in headers override these...
+
+        let canonicalRequest = createCanonicalRequest(method, path, querystring, reqHeaders, payload);
+        let stringToSign = createStringToSign(timestamp, region, service, canonicalRequest);
+        let signingKey = createSigningKey(secretKey, datestamp, region, service);
+        let signature = hmacSha256(stringToSign, signingKey, true);
+        let authHeader = createAuthHeader(accessKey, timestamp, region, service, reqHeaders, signature);
+
+        reqHeaders.Authorization = authHeader;
+
+        //Now, lets finally do a HTTP REQUEST!!!
+        request(method, path, reqHeaders, querystring, payload, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+function createResult(data, res) {
+    if (!data || data.length === 0) {
+        return { statusCode: res.statusCode, headers: res.headers, data:''};
+    }
+    if (data && data.length > 0 && res.headers['content-type'] === 'application/json') {
+        return { statusCode : res.statusCode, headers: res.headers, data : JSON.parse(data)};
+    } else {
+        return { statusCode : res.statusCode, headers: res.headers, data};
+    }
+}
+
+function request(method, path, headers, querystring, data, callback) {
+    
+    let qs = Object.keys(querystring).map(k => `${k}=${querystring[k]}`).join('&');
+    path += '?' + qs;
+    let hostname = headers.Host;
+    delete headers.Host;
+    headers['Content-Length'] = data.length;
+    const port = 443;
+    try {
+        const options = { hostname, port, path, method, headers };
+        const req = https.request(options, res => {
+    
+            let chunks = [];
+            res.on('data', d => chunks.push(d));
+            res.on('end', () => {
+                let buffer = Buffer.concat(chunks);
+                if (res.headers['content-encoding'] === 'gzip') {
+                    zlib.gunzip(buffer, (err, decoded) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(null, createResult(decoded, res));
+                        }
+                    });
+                } else {
+                    callback(null, createResult(buffer, res));
+                }
+            });
+    
+        });
+        req.on('error', err => callback(err));
+
+        if (data) {
+            req.write(data);
+        }
+        req.end();
+    } catch(err) {
+        callback(err);
+    }
+}
+
+module.exports = awsApiRequest;
---
+++ beanstalk-deploy.js
@@ -0,0 +1,238 @@
+#!/usr/bin/env node
+// Author: Einar Egilsson, https://github.com/einaregilsson/beanstalk-deploy
+
+const awsApiRequest = require('./aws-api-request');
+const fs = require('fs');
+
+const IS_GITHUB_ACTION = !!process.env.GITHUB_ACTION;
+
+if (IS_GITHUB_ACTION) {
+    console.error = msg => console.log(`::error::${msg}`);
+    console.warn = msg => console.log(`::warning::${msg}`);
+}
+
+function createStorageLocation() {
+    return awsApiRequest({
+        service: 'elasticbeanstalk',
+        querystring: {Operation: 'CreateStorageLocation', Version: '2010-12-01'}
+    });
+}
+
+function checkIfFileExistsInS3(bucket, s3Key) {
+    return awsApiRequest({
+        service : 's3', 
+        host: `${bucket}.s3.amazonaws.com`,
+        path : s3Key,
+        method: 'HEAD'
+    });
+}
+
+function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
+function uploadFileToS3(bucket, s3Key, filebuffer) {
+    return awsApiRequest({
+        service : 's3', 
+        host: `${bucket}.s3.amazonaws.com`,
+        path : s3Key,
+        method: 'PUT',
+        headers: { 'Content-Type' : 'application/octet-stream'},
+        payload: filebuffer
+    });
+}
+
+function createBeanstalkVersion(application, bucket, s3Key, versionLabel) {
+    return awsApiRequest({
+        service: 'elasticbeanstalk',
+        querystring: {
+            Operation: 'CreateApplicationVersion', 
+            Version: '2010-12-01',
+            ApplicationName : application,
+            VersionLabel : versionLabel,
+            'SourceBundle.S3Bucket' : bucket,
+            'SourceBundle.S3Key' : s3Key.substr(1) //Don't want leading / here
+        }
+    });
+}
+
+function deployBeanstalkVersion(application, environmentName, versionLabel) {
+    return awsApiRequest({
+        service: 'elasticbeanstalk',
+        querystring: {
+            Operation: 'UpdateEnvironment', 
+            Version: '2010-12-01',
+            ApplicationName : application,
+            EnvironmentName : environmentName,
+            VersionLabel : versionLabel
+        }
+    });
+}
+
+function describeEvents(application, environmentName, startTime) {
+    return awsApiRequest({
+        service: 'elasticbeanstalk',
+        querystring: {
+            Operation: 'DescribeEvents', 
+            Version: '2010-12-01',
+            ApplicationName : application,
+            Severity : 'TRACE',
+            EnvironmentName : environmentName,
+            StartTime : startTime.toISOString().replace(/(-|:|\.\d\d\d)/g, '')
+        }
+    });
+}
+
+function describeEnvironments(application, environmentName) {
+    return awsApiRequest({
+        service: 'elasticbeanstalk',
+        querystring: {
+            Operation: 'DescribeEnvironments', 
+            Version: '2010-12-01',
+            ApplicationName : application,
+            'EnvironmentNames.members.1' : environmentName //Yes, that's the horrible way to pass an array...
+        }
+    });
+}
+
+function expect(status, result) {
+    if (status !== result.statusCode) { 
+        if (result.headers['content-type'] !== 'application/json') {
+            throw new Error(`Status: ${result.statusCode}. Message: ${result.data}`);
+        } else {
+            throw new Error(`Status: ${result.statusCode}. Code: ${result.data.Error.Code}, Message: ${result.data.Error.Message}`);
+        }
+    }
+}
+
+function main() {
+
+    let application, environmentName, versionLabel, region, file;
+    if (IS_GITHUB_ACTION) { //Running in GitHub Actions
+        application = process.env.INPUT_APPLICATION_NAME;
+        environmentName = process.env.INPUT_ENVIRONMENT_NAME;
+        versionLabel = process.env.INPUT_VERSION_LABEL;
+        file = process.env.INPUT_DEPLOYMENT_PACKAGE;
+
+        awsApiRequest.accessKey = process.env.INPUT_AWS_ACCESS_KEY;
+        awsApiRequest.secretKey = process.env.INPUT_AWS_SECRET_KEY;
+        awsApiRequest.region = process.env.INPUT_REGION;
+
+    } else { //Running as command line script
+        if (process.argv.length < 6) {
+            console.log('\nbeanstalk-deploy: Deploy a zip file to AWS Elastic Beanstalk');
+            console.log('https://github.com/einaregilsson/beanstalk-deploy\n');
+            console.log('Usage: beanstalk-deploy.js <application> <environment> <versionLabel> <region> [<filename>]\n');
+            console.log('Environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be defined for the program to work.')
+            console.log('If <filename> is skipped there will be no deployment, the script will simply log events from the environment.\n');
+            process.exit(1);
+        }
+
+        [application, environmentName, versionLabel, region, file] = process.argv.slice(2);
+
+        awsApiRequest.accessKey = process.env.AWS_ACCESS_KEY_ID;
+        awsApiRequest.secretKey = process.env.AWS_SECRET_ACCESS_KEY;
+        awsApiRequest.region = region;
+    }
+
+    let s3Key = `/${application}/${versionLabel}.zip`;
+    let bucket, deployStart, fileBuffer;
+
+    if (!file && !IS_GITHUB_ACTION) { //when something fails during the monitoring, make it possible to just observe what's going on. Start 5 minutes ago...
+        let fiveMinutesAgo = new Date(new Date().getTime() - 5 * 60 * 1000);
+        waitForDeployment(application, environmentName, versionLabel, fiveMinutesAgo);
+        return;
+    }
+
+    readFile(file).then(result => {
+        fileBuffer = result;
+        return createStorageLocation(region);
+    }).then(result => {
+        expect(200, result );
+        bucket = result.data.CreateStorageLocationResponse.CreateStorageLocationResult.S3Bucket;
+        console.log(`Uploading file to bucket ${bucket}`);
+        return checkIfFileExistsInS3(bucket, s3Key);
+    }).then(result => {
+        if (result.statusCode === 200) {
+            throw new Error(`Version ${versionLabel} already exists in S3!`);
+        }
+        expect(404, result); 
+        return uploadFileToS3(bucket, s3Key, fileBuffer);
+    }).then(result => {
+        expect(200, result);
+        console.log(`New build successfully uploaded to S3, bucket=${bucket}, key=${s3Key}`);
+        return createBeanstalkVersion(application, bucket, s3Key, versionLabel);
+    }).then(result => {
+        expect(200, result);
+        console.log(`Created new application version ${versionLabel} in Beanstalk.`);
+        deployStart = new Date();
+        console.log(`Starting deployment of version ${versionLabel} to environment ${environmentName}`);
+        return deployBeanstalkVersion(application, environmentName, versionLabel);
+    }).then(result => {
+        expect(200, result);
+        console.log('Deployment started...\n');
+
+        return waitForDeployment(application, environmentName, versionLabel, deployStart);
+
+    }).then(envAfterDeployment => {
+        if (envAfterDeployment.Health === 'Green') {
+            console.log('Environment update successful!')
+            process.exit(0);
+        } else {
+            console.warn(`Update finished, but environment health is: ${envAfterDeployment.Health}, HealthStatus: ${envAfterDeployment.HealthStatus}`);
+            process.exit(1);
+        }
+    }).catch(err => {
+        console.error(`Deployment failed: ${err}`);
+        process.exit(2);
+    }); 
+}
+
+//Wait until the new version is deployed, printing any events happening during the wait...
+function waitForDeployment(application, environmentName, versionLabel, start) {
+    let counter = 0;
+    return new Promise((resolve, reject) => {
+        function update() {
+            describeEvents(application, environmentName, start).then(result => {
+                expect(200, result);
+                let events = result.data.DescribeEventsResponse.DescribeEventsResult.Events.reverse(); //They show up in desc, we want asc for logging...
+                for (let ev of events) {
+                    let date = new Date(ev.EventDate * 1000); //Seconds to milliseconds,
+                    console.log(`${date.toISOString().substr(11,8)} ${ev.Severity}: ${ev.Message}`);
+                }
+                if (events.length > 0) {
+                    start = new Date(events[events.length-1].EventDate * 1000 + 1000); //Add extra second so we don't get the same message next time...
+                }
+            }).catch(reject);
+    
+            describeEnvironments(application, environmentName).then(result => {
+                expect(200, result);
+                counter++;
+                let env = result.data.DescribeEnvironmentsResponse.DescribeEnvironmentsResult.Environments[0];
+                if (env.VersionLabel === versionLabel && env.Status === 'Ready') {
+                    console.log(`Deployment finished. Version updated to ${env.VersionLabel}`);
+                    console.log(`Status for ${application}-${environmentName} is ${env.Status}, Health: ${env.Health}, HealthStatus: ${env.HealthStatus}`);
+                    resolve(env);
+                } else {
+                    if (counter % 6 === 0) {
+                        console.log(`${new Date().toISOString().substr(11,8)} INFO: Still updating, status is "${env.Status}", health is "${env.Health}", health status is "${env.HealthStatus}"`);
+                    }
+                    setTimeout(update, 5000);
+                }
+            }).catch(reject);
+        };
+    
+        update();
+    });
+}
+
+main();
+
+
